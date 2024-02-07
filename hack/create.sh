367e8e20f3f1d9dca502bdbb7eacf31d74a5aa42
nfyxhan <nfyxhan@163.com>
2024-02-07 17:33:32 +0800

    update

--- hack/create.sh
+++ hack/create.sh
@@ -12,6 +12,8 @@ if [[ "${haslogin}" == "" ]]; then
     docker login ${REGISTRY}
 fi
 
+sed -i s'#kindest-node:.*#kindest-node:'${KINDEST_NODE_VERSION}'#'g ${CONFIG_FILE}
+
 images=`cat ${CONFIG_FILE} | grep image | awk '{print $2}'`
 for i in $images ; do
    docker pull $i
@@ -20,7 +22,6 @@ done
 HOST_IP=`ip addr|grep eth0 | grep inet | awk '{print $2}'|awk -F '/' '{print $1}'`
 
 sed -i s'#apiServerAddress.*#apiServerAddress: '${HOST_IP}'#'g ${CONFIG_FILE} 
-sed -i s'#kindest-node:.*#kindest-node:'${KINDEST_NODE_VERSION}'#'g ${CONFIG_FILE} 
 
 kind create cluster --config ${CONFIG_FILE} \
    --kubeconfig="${KUBECONFIG}" $@
